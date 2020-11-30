086947226e716013993d64dd13dc62f215c2d9bb
sferubko <sferubko@gmail.com>
2020-12-01 01:01:41 +0200

    refactored

--- fpl-client/src/main/java/com/svf/fpl/edu/config/FplClientConfiguration.java
+++ fpl-client/src/main/java/com/svf/fpl/edu/config/FplClientConfiguration.java
@@ -50,11 +50,6 @@ public class FplClientConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public FantasyPremierLeagueClient fplClient() {
-//        return new FantasyPremierLeagueClient();
-//    }
-
     @Bean
     public CacheManager cacheManager() {
         return new ConcurrentMapCacheManager("players", "fullInformation") {
@@ -73,61 +68,4 @@ public class FplClientConfiguration {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedMethods("*")
-//                .allowedOrigins("*")
-//                .allowedHeaders("*");
-//    }
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        UserDetailsService userDetailsService = getUserDetails();
-//        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-//    }
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.httpBasic().disable().csrf().disable().sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and().authorizeRequests()
-//                .antMatchers("/api/auth/login").permitAll()
-//                .antMatchers("/api/auth/register").permitAll()
-//                .antMatchers("/player/api/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and().csrf()
-//                .disable().exceptionHandling()
-//                .authenticationEntryPoint(unauthorizedEntryPoint()).and()
-//                .apply(new JwtConfigurer(jwtTokenProvider));
-//        http.cors();
-//    }
-
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
-//    }
-
-//    @Bean
-//    public PasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-//
-//    @Bean
-//    public AuthenticationEntryPoint unauthorizedEntryPoint() {
-//        return (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-//                "Unauthorized");
-//    }
-//
-//    @Bean
-//    public UserDetailsService getUserDetails() {
-//        return new CustomUserDetailsService();
-//    }
 }
