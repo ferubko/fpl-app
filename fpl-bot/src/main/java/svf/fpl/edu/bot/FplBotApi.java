aa06d4daa2ed1d4577d4999ee0cd2b5e598c267c
sferubko <sferubko@gmail.com>
2020-12-01 22:18:24 +0200

    refactored

--- fpl-bot/src/main/java/svf/fpl/edu/bot/FplBotApi.java
+++ fpl-bot/src/main/java/svf/fpl/edu/bot/FplBotApi.java
@@ -2,14 +2,21 @@ package svf.fpl.edu.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 @EnableScheduling
-public class FplBotApi {
+public class FplBotApi extends SpringBootServletInitializer {
     public static void main(String[] args) {
         ApiContextInitializer.init();
         SpringApplication.run(FplBotApi.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FplBotApi.class);
     }
 }
